d620f1755b429fc3344fbceaec997d1060ebe03c
Marius Bock <marius.bock@gmail.com>
2025-07-16 13:25:02 +0200

    Updated view script to also show timestamp

--- view_balance.sh
+++ view_balance.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-grep "Balance" bitcoin_balance.log | grep -v "Balance: 0 BTC"
+# filepath: /Users/mariusbock/LocalDocuments/Development/Rust/bitcoin_addr/view_balance.sh
+
+grep -E "Balance|^Start time:" bitcoin_balance.log | grep -v "Balance: 0 BTC"
