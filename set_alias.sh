b370f7b244893d0101aa09c5f0b7102a002462ad
Marius Bock <marius.bock@gmail.com>
2025-08-02 11:33:02 +0200

    New script to set alias for all the view scripts

---
+++ set_alias.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# filepath: /Users/mariusbock/LocalDocuments/Development/Rust/bitcoin_addr/set_alias.sh
+
+alias ve=./view_error.sh
+alias vb=./view_balance.sh
+alias vf=./view_found.sh
