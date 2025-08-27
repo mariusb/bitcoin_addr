00aaae481768bc6e911d23cba76d470c7934ecaf
Marius Bock <marius.bock@gmail.com>
2025-08-27 14:10:11 +0200

    fix: Add header and column formatting to summary queries in summary.sh

--- summary.sh
+++ summary.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ../btc-addr-summary/target/release/btc-addr-summary
-sqlite3 summary.db "SELECT * FROM summary;"
-sqlite3 summary.db "SELECT sum(without_balance) FROM summary;"
+sqlite3 -header -column summary.db "SELECT * FROM summary;"
+sqlite3 -header -column summary.db "SELECT sum(without_balance) FROM summary;"
